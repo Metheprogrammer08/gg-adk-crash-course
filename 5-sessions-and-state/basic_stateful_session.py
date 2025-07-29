2d1aeac4a10355613761b82c7b54869dfafed795
Metheprogrammer08 <donhuhai03062008@gmail.com>
2025-07-29 15:32:48 +0700

    Final modification to 5-sessions-and-state

--- 5-sessions-and-state/basic_stateful_session.py
+++ 5-sessions-and-state/basic_stateful_session.py
@@ -55,15 +55,15 @@ async def main():
             if event.content and event.content.parts:
                 print(f"Final Response: {event.content.parts[0].text}")
 
-    # print("==== Session Event Exploration ====")
-    # session = session_service_stateful.get_session(
-    #     app_name=APP_NAME, user_id=USER_ID, session_id=SESSION_ID
-    # )
+    print("==== Session Event Exploration ====")
+    session = await session_service_stateful.get_session(
+        app_name=APP_NAME, user_id=USER_ID, session_id=SESSION_ID
+    )
 
-    # # Log final Session state
-    # print("=== Final Session State ===")
-    # for key, value in session.State.items():
-    #     print(f"{key}: {value}")
+    # Log final Session state
+    print("=== Final Session State ===")
+    for key, value in session.state.items():
+        print(f"{key}: {value}")
 
 if __name__ == "__main__":
     # load_dotenv()
